5ecb934cbd6494a4e19d95753f00938da4636fc8
Tiago Medeiros <85853285+Tiagomedeirosds@users.noreply.github.com>
2021-12-13 17:05:29 -0500

    about and Home completed

--- nutritionista/main_app/views.py
+++ nutritionista/main_app/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Home </h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
