fd8f59760f6679d643596691d644e422fb7531dd
Omar Hawas <omarhawas@Omars-MacBook-Air.local>
2021-12-13 17:14:18 -0500

    first authorization commit

--- nutritionista/main_app/models.py
+++ nutritionista/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Day(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'day_id': self.id})
