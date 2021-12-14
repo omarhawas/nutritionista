45d763867b1bfb1d56b693c4febf2acc9e57077f
Omar Hawas <omarhawas@Omars-MacBook-Air.local>
2021-12-14 11:38:29 -0500

    authorization completed

--- nutritionista/main_app/views.py
+++ nutritionista/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -14,23 +14,25 @@ def about(request):
     return render(request, 'about.html')
 
 def days_index(request):
-    return render(request, 'days/index.html', {'days': days })
+    return render(request, 'days/index.html') #{'days': days })
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    try:
+        if request.method == 'POST':
+            # This is how to create a 'user' form object
+            # that includes the data from the browser
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                # This will add the user to the database
+                user = form.save()
+                # This is how we log a user in via code
+                login(request, user)
+                return redirect('index')
+            else:
+                error_message = 'Invalid sign up - try again'
+    except Exception as err:
+        print(err)
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
