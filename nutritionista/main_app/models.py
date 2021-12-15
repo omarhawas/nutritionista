d2b188ecaab38cb933197d80002cd455a9408486
Tiago Medeiros <85853285+Tiagomedeirosds@users.noreply.github.com>
2021-12-14 19:30:07 -0500

    Foods CRUD completed

--- nutritionista/main_app/models.py
+++ nutritionista/main_app/models.py
@@ -16,3 +16,13 @@ class Day(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'day_id': self.id})
 
+class Food(models.Model):
+  name = models.CharField(max_length=50)
+  calories = models.IntegerField()
+  portion = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('foods_detail', kwargs={'pk': self.id})  
--- nutritionista/main_app/urls.py
+++ nutritionista/main_app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('days/', views.days_index, name='index'),
     path('days/<int:day_id>/', views.days_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('foods/', views.FoodList.as_view(), name='foods_index'),
+    path('foods/<int:pk>/', views.FoodDetail.as_view(), name='foods_detail'),
+    path('foods/create/', views.FoodCreate.as_view(), name='foods_create'),
+    path('foods/<int:pk>/update', views.FoodUpdate.as_view(), name='foods_update'),
+    path('foods/<int:pk>/delete', views.FoodDelete.as_view(), name='foods_delete'),
+
 ]
